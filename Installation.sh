99e836aa764cca9fe1b060f19920b05fa2585ad6
SHIRE <204878993@qq.com>
2020-02-08 19:45:27 +0800

    Edit locales

--- Installation.sh
+++ Installation.sh
@@ -14,13 +14,10 @@ passwd
 ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
 
 # Set locales and generate them
-echo "LANG=de_DE.UTF-8" > /etc/locale.conf
-echo "LANGUAGE=de_DE" >> /etc/locale.conf
-echo "KEYMAP=de-latin1" > /etc/vconsole.conf
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+echo "LANGUAGE=en_US" >> /etc/locale.conf
 echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
 echo "en_US ISO-8859-1" >> /etc/locale.gen
-echo "de_DE.UTF-8 UTF-8" >> /etc/locale.gen
-echo "de_DE ISO-8859-1" >> /etc/locale.gen
 locale-gen
 
 # Fix pacman: Signature is unknown trust
